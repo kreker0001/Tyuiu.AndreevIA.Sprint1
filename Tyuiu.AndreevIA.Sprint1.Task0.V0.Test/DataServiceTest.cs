12684faa8ac8f24d01d70c16e4e96a7d2d522eed
KREKER <51415630+kreker0001@users.noreply.github.com>
2025-09-21 17:48:49 +0500

    Add project files.

---
+++ Tyuiu.AndreevIA.Sprint1.Task0.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.AndreevIA.Sprint1.Task0.V10.Lib;
+
+namespace Tyuiu.AndreevIA.Sprint1.Task0.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(-10.5, res);
+
+        }
+    }
+}
---
+++ Tyuiu.AndreevIA.Sprint1.Task0.V0/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.AndreevIA.Sprint1.Task0.V10.Lib;
+
+namespace Tyuiu.AndreevIA.Sprint1.Task0.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #1 | Выполнил Андреев И. А. | РППб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема. Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Андреев Иван Александрович | РППб-25-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение (3/6-4)*3               *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* (3/6-4)*3                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+        }
+    }
+}
