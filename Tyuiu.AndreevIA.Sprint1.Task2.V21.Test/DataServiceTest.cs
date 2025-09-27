e5a5a6d182d07b98609e9a1b7d16ad8f5a216976
KREKER <51415630+kreker0001@users.noreply.github.com>
2025-09-28 00:29:53 +0500

    Task2

---
+++ Tyuiu.AndreevIA.Sprint1.Task2.V21.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AndreevIA.Sprint1.Task2.V21.Lib;
+
+namespace Tyuiu.AndreevIA.Sprint1.Task2.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            int y = 2;
+            var res = ds.CalculateRectangleSquare(x,y);
+            Assert.AreEqual(4, res);
+        }
+    }
+}
---
+++ Tyuiu.AndreevIA.Sprint1.Task2.V21/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.AndreevIA.Sprint1.Task2.V21.Lib;
+
+namespace Tyuiu.AndreevIA.Sprint1.Task1.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил Андреев И. А. | РППб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема. Арифметические операторы в C#                                     *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Андреев Иван Александрович | РППб-25-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Известны длины стороны прямоугольника. Вычислить площадь прямоугольника *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x, y;
+
+            Console.WriteLine("Длина первой стороны прямоугольника (целое число):");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Длина второй стороны прямоугольника (целое число):");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Площадь прямоугольника (целое число): " + ds.CalculateRectangleSquare(x,y));
+
+            Console.ReadLine();
+        }
+    }
+}
